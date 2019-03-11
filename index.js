015970b7a3c9ad57671d16ad338159f70c35ef50
Israel Michael <israelfcfm@outlook.com>
2019-03-11 11:32:27 -0300

    added output style package

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 
-const { ApolloServer, gql } = require('apollo-server');
+const {
+	ApolloServer,
+	gql
+} = require('apollo-server');
 
 //config of server
 const config = require('./config');
@@ -10,38 +13,49 @@ const Connection = require('./connection/index');
 const resolvers = require('./resolvers/index');
 const schema = require('./schemas/index');
 
-const { output, outputln } = require('./utils/output');
+const {
+	output
+} = require('outputjsc');
 
 const typeDefs = gql(`
     ${schema.Query}
     ${schema.Mutation}
 `);
 
-const Start = async () => {    
-    
-    //define the environment - default is development
-    const env = process.env.NODE_ENV = config.NODE_ENV || 'development';
-    
-    const db = await Connection(/* params of conexion*/);
+const Start = async () => {
 
-    const defs = {
-        typeDefs,
-        resolvers: resolvers(schema.Mutation !== ''),
-        playground: config[env].serveroptions.playground,
-        context: ({req}) => ( { db } ),
-    };
+	//define the environment - default is development
+	const env = process.env.NODE_ENV = config.NODE_ENV || 'development';
 
-    const server = new ApolloServer(defs);
+	const db = await Connection( /* params of conexion*/ );
 
-    server.listen({port: config[env].serveroptions.port || 3000, host: config[env].serveroptions.host || 'localhost'})
-    .then(infos => {
-        output(`server is started at port ${infos.port}`, "cyan");
-        output(`the server is acessible on: ${infos.url}`, 'green');
-    })
-    .catch(err => {
-        throw err;
-    });
-    
+	const defs = {
+		typeDefs,
+		resolvers: resolvers(schema.Mutation !== ''),
+		playground: config[env].serveroptions.playground,
+		context: ({
+			req
+		}) => ({
+			db
+		}),
+	};
+
+	const server = new ApolloServer(defs);
+
+	server.listen({
+			port: config[env].serveroptions.port || 3000,
+			host: config[env].serveroptions.host || 'localhost'
+		})
+		.then(infos => {
+			output
+				.cyan(`server is started at port ${infos.port}`)
+				.ln()
+				.green(`the server is acessible on: ${infos.url}`);
+		})
+		.catch(err => {
+			throw err;
+		});
+
 };
 
 //start the server
